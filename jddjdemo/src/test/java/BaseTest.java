a0c2e7714eebe7edfe5e243952af1a69a2d92606
zhengxu <18281604336@163.com>
2019-03-27 17:37:36 +0800

    修改乱码

---
+++ jddjdemo/src/test/java/BaseTest.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 测试基类
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class BaseTest {
+}
