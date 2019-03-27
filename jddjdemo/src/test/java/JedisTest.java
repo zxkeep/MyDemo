e7de82c62d8f8bf14f22cb73b36221fbc7cf4dd2
zxkeep <18281604336@163.com>
2019-03-28 00:04:10 +0800

    www

--- jddjdemo/src/test/java/JedisTest.java
+++ jddjdemo/src/test/java/JedisTest.java
@@ -3,6 +3,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.List;
+import java.util.Set;
+
 
 public class JedisTest extends BaseTest{
 
@@ -15,5 +18,11 @@ public class JedisTest extends BaseTest{
         Jedis jedis = jedisPool.getResource();
         String s = jedis.get("hobby");
         System.out.println(s);
+        jedis.set("name", "keep");
+        List<String> list = jedis.mget("name", "hobby", "birthday");
+        System.out.println(list);
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
+
     }
 }
