5f28657cee99f15ff5737ececbd66cc4161175d9
zhengxu <18281604336@163.com>
2019-03-29 14:04:30 +0800

    springdataredis

--- jddjdemo/src/test/java/JedisTest.java
+++ jddjdemo/src/test/java/JedisTest.java
@@ -14,54 +14,54 @@ import java.util.Set;
 public class JedisTest extends BaseTest{
 
     //@Autowired
-    private JedisPool jedisPool;
+   // private JedisPool jedisPool;
 
     @Autowired
     private RedisCacheManager redisCacheManager;
 
-    @Test
-    public void  testJedis(){
-
-        Jedis jedis = jedisPool.getResource();
-        String s = jedis.get("hobby");
-        System.out.println(s);
-        jedis.set("name", "keep");
-        List<String> list = jedis.mget("name", "hobby", "birthday");
-        System.out.println(list);
-        Set<String> keys = jedis.keys("*");
-        System.out.println(keys);
-        System.out.println("===================================================");
-        //StringBuilder sbKey=new StringBuilder();
-        ArrayList values = new ArrayList<>();
-        for (String key:keys) {
-            String value = jedis.get(key);
-            values.add(value);
-        }
-//        String allKeys = sbKey.substring(0, sbKey.length() - 1).toString();
-//        System.out.println("-----------"+allKeys);
-//        System.out.println("-----------"+sbKey);
-//        List<String> values = jedis.mget(allKeys);
-        System.out.println("-----------"+values);
-
-
-    }
-
-    @Test
-    public void testAddJsonStr(){
-        Jedis jedis = jedisPool.getResource();
-        User user = new User();
-        user.setExpires_in(222323L);
-        user.setTime(33424L);
-        user.setToken("234567876543");
-        user.setUid(4L);
-        user.setUser_nick("keep");
-        user.setVenderId(201890328L);
-        String value = String.valueOf(user);
-        System.out.println("============"+value);
-        String json = JSONObject.toJSONString(user);
-        System.out.println(json);
-        jedis.set(user.getUid().toString(),json);
-    }
+//    @Test
+//    public void  testJedis(){
+//
+//        Jedis jedis = jedisPool.getResource();
+//        String s = jedis.get("hobby");
+//        System.out.println(s);
+//        jedis.set("name", "keep");
+//        List<String> list = jedis.mget("name", "hobby", "birthday");
+//        System.out.println(list);
+//        Set<String> keys = jedis.keys("*");
+//        System.out.println(keys);
+//        System.out.println("===================================================");
+//        //StringBuilder sbKey=new StringBuilder();
+//        ArrayList values = new ArrayList<>();
+//        for (String key:keys) {
+//            String value = jedis.get(key);
+//            values.add(value);
+//        }
+////        String allKeys = sbKey.substring(0, sbKey.length() - 1).toString();
+////        System.out.println("-----------"+allKeys);
+////        System.out.println("-----------"+sbKey);
+////        List<String> values = jedis.mget(allKeys);
+//        System.out.println("-----------"+values);
+//
+//
+//    }
+//
+//    @Test
+//    public void testAddJsonStr(){
+//        Jedis jedis = jedisPool.getResource();
+//        User user = new User();
+//        user.setExpires_in(222323L);
+//        user.setTime(33424L);
+//        user.setToken("234567876543");
+//        user.setUid(4L);
+//        user.setUser_nick("keep");
+//        user.setVenderId(201890328L);
+//        String value = String.valueOf(user);
+//        System.out.println("============"+value);
+//        String json = JSONObject.toJSONString(user);
+//        System.out.println(json);
+//        jedis.set(user.getUid().toString(),json);
+//    }
 
     @Test
     public void getAll(){
