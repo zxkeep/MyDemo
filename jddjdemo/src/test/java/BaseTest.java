76b11ab3b0792707a1e1e26b57fcf63a67f28696
zxkeep <18281604336@163.com>
2019-05-05 00:56:01 +0800

    解决swagger与junit 兼容问题

--- jddjdemo/src/test/java/BaseTest.java
+++ jddjdemo/src/test/java/BaseTest.java
@@ -2,11 +2,13 @@ import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * 测试基类
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration("classpath:applicationContext.xml")
 public class BaseTest {
 }
