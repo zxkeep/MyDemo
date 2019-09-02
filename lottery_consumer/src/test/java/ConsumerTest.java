efe7359b1d40181c49338592aa2fc75b8a753aa4
zxkeep <18281604336@163.com>
2019-09-02 22:23:46 +0800

    huitui

--- lottery_consumer/src/test/java/ConsumerTest.java
+++ lottery_consumer/src/test/java/ConsumerTest.java
@@ -5,8 +5,6 @@ public class ConsumerTest extends BaseTest{
     public void testConsumerMessage() {
         while (true) {
             // junit退出，防止进程死掉
-            System.out.println("ddddddddddddddddddddd");
-
         }
     }
 }
