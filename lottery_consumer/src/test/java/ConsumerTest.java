d9063e8027da5631eab47042859b317334042395
zxkeep <18281604336@163.com>
2019-09-02 22:22:55 +0800

    ceshi

--- lottery_consumer/src/test/java/ConsumerTest.java
+++ lottery_consumer/src/test/java/ConsumerTest.java
@@ -5,6 +5,8 @@ public class ConsumerTest extends BaseTest{
     public void testConsumerMessage() {
         while (true) {
             // junit退出，防止进程死掉
+            System.out.println("ddddddddddddddddddddd");
+
         }
     }
 }
